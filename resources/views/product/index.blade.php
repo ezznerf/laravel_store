519857ae09b25a1096256aeb68ea58fd31fea74a
ezznerf <yakuta04@mail.ru>
2023-11-03 18:48:16 +0300

    alpha index.blade.php

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,62 +1,59 @@
 @extends('layouts.index')
 @section('content')
 <div>
-    <div class="container text-center" >
+    <div class="container text-center me-2" >
 
         <div class="row col-5" style="padding-right: 0px">
-            <div class="col bg-light p-5 border mb-4 rounded" style="margin-top: 24%">
+            <div class="col bg-light p-4 border mb-4 rounded" style="margin-top: 24%">
                 <h1>СМАРТФОНЫ</h1>
 
             </div>
 
         </div>
         <div class="row col-5">
-            <div class="col bg-light p-5 border mb-4 rounded">
+            <div class="col bg-light p-4 border mb-4 rounded">
                 <h1>ТЕЛЕФОНЫ</h1>
             </div>
         </div>
         <div class="row col-5">
-            <div class="col bg-light p-5 border mb-4 rounded">
+            <div class="col bg-light p-4 border mb-4 rounded">
                 <h1>ПЛАНШЕТЫ</h1>
             </div>
         </div>
         <div class="row col-5">
-            <div class="col bg-light p-5 border mb-4 rounded">
+            <div class="col bg-light p-4 border mb-4 rounded">
                 <h1>СМАРТ-ЧАСЫ</h1>
             </div>
         </div>
 
-        <div class="col-4 rounded" style="position: absolute; right: 16%; top: 12%; height: 61.9% ;background-color: #0a3622">
+        <div class="col-4 rounded" style="position: absolute; right: 10%; top: 16%; height: 30rem ;background-color: #0a3622">
 
-            <div class="card border-danger mb-3" style="max-width: 100%; height: 25%">
-                <div class="card-header">АКЦИЯ</div>
+            <div class="card border-danger mb-3" style="max-width: 100%; height: 10rem;">
+                <div class="card-header">Cкидки</div>
                 <div class="card-body text-danger">
                     <h5 class="card-title">Черная пятница!</h5>
-                    <p class="card-text">Срочно оформите заказ самовывозом или купитете в нашем физическом магазине в эту пятницу и он получите выгоду до 50% </p>
+                    <p class="card-text">Срочно оформите заказ самовывозом или купитете в нашем физическом магазине в эту пятницу и получите выгоду до 50% </p>
                 </div>
 
             </div>
-            <div class="card border-danger mb-3" style="max-width: 100%; height: 15%">
-                <div class="card-header">НОВОЕ</div>
-                <div class="card-body text-danger">
-                    <h5 class="card-title">Новоинки последней недели</h5>
-                </div>
-        </div>
 
-            <div class="card border-danger mb-3" style="max-width: 48%; height: 55.1%">
-                <div class="card-header">Название продукта</div>
+            <div class="card border-danger mb-3" style="max-width: 100%; height: 8rem;">
+                <div class="card-header">Выгодное предложение</div>
                 <div class="card-body text-danger">
-                    <h5 class="card-title">Фото</h5>
+                    <p class="card-text">При покупке любого товара бренда POCO получите видеокарту GeForce RTX 2040 </p>
                 </div>
 
             </div>
-            <div class="card border-danger mb-3" style="max-width: 48%; height: 55.3%; left: 52%; bottom:57.6%">
-                <div class="card-header">Название продукта</div>
+
+            <div class="card border-danger mb-3" style="max-width: 100%; height: 10rem;">
+                <div class="card-header">Акция</div>
                 <div class="card-body text-danger">
-                    <h5 class="card-title">Фото</h5>
+                    <h5 class="card-title">1+1=2</h5>
+                    <p class="card-text">Если вы купите или закажите один любой товар в нашем магазине то получите второй товар по его же цене в подарок !</p>
                 </div>
 
             </div>
+
             <div class="col-12 rounded" style="position: absolute; background-color: #0dcaf0; bottom: 103%; height: 5%">Корзина</div>
     </div>
 </div>
